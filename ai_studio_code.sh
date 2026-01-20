944982ab1851e1b261878ac9befbe6915a2fa676
Kauan2012dev <oficialglx.01@gmail.com>
2026-01-20 11:48:00 -0300

    Add files via upload

---
+++ ai_studio_code.sh
@@ -0,0 +1,14 @@
+# Inicie o git na pasta do projeto
+git init
+
+# Adicione todos os arquivos
+git add .
+
+# Crie o primeiro registro
+git commit -m "Initial commit - Glx.01 Virtual Core"
+
+# Conecte ao seu link (substitua pelo link que você criou no passo 2)
+git remote add origin https://github.com/SEU_USUARIO/NOME_DO_REPOSITORIO.git
+
+# Envie os arquivos
+git push -u origin main
